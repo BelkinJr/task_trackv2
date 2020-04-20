95bb1c7883c7d527881c81cc665ee4dacdff00a2
Igor <uggoth@gmail.com>
2020-04-20 12:29:03 +0300

    Fix team model import

--- team/models/team.py
+++ team/models/team.py
@@ -3,12 +3,10 @@ from base.models import BaseModel
 from user.models import User
 from team.models.userteam import UserTeamRelation
 
-# Create your models here.
 
 class Team (BaseModel):
 
     team_name = models.CharField(max_length=255)
-    uesr_count = models.IntegerField()
     users = models.ManyToManyField(User, related_name='teams', through=UserTeamRelation)
 
     class Meta:
--- team/models/userteam.py
+++ team/models/userteam.py
@@ -1,11 +1,11 @@
 from django.db import models
 from base.models import BaseModel
 from user.models import User
-from team.models.team import Team
+
 
 class UserTeamRelation (BaseModel):
 
-    team = models.ForeignKey(Team, related_name='team_users',  on_delete=models.CASCADE)
+    team = models.ForeignKey('Team', related_name='team_users',  on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='user_teams', on_delete=models.CASCADE)
 
     class Meta:
