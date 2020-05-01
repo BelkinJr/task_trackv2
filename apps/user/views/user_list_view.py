dc4ee95698b49448af674beb61ebaa8572529546
Vitaly <belkin.jr.nvk@gmail.com>
2020-05-01 15:13:13 +1200

    notes fixed

--- apps/user/views/user_list_view.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from apps.user.models import User
-from apps.user.serializers.user_detail_serializer import UserDetailSerializer
-
-
-class UserListView(generics.GenericAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserDetailSerializer
