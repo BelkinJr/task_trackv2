7bab1ac8b721fc27c82eb9919070d4e2d5c6d402
Vitaly <belkin.jr.nvk@gmail.com>
2020-05-29 21:31:47 +1200

    invite

---
+++ apps/invite/models/invite.py
@@ -0,0 +1,22 @@
+from django.db import models
+from apps.user.models import User
+from apps.team.models import Team
+from apps.base.models.base import BaseModel
+
+
+class Invite(BaseModel):
+    INVITE_STATUS_ACCEPTED = 'ACC'
+    INVITE_STATUS_DECLINED = 'DEC'
+    INVITE_STATUS_PENDING = 'PEN'
+
+    user = models.ForeignKey(User, related_name='invite', on_delete=models.DO_NOTHING)
+    team = models.ForeignKey(Team, related_name='invite', on_delete=models.DO_NOTHING)
+    status = models.CharField(choices=(
+        (INVITE_STATUS_ACCEPTED, 'accepted'),
+        (INVITE_STATUS_DECLINED, 'declined'),
+        (INVITE_STATUS_PENDING, 'pending'),
+    ), default=INVITE_STATUS_PENDING, max_length=31)
+
+    class Meta:
+        db_table = 'invite'
+        verbose_name = 'invite'
---
+++ apps/invite/views/invite_view.py
@@ -0,0 +1,27 @@
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+from apps.invite.models.invite import Invite
+from apps.user.models.user import User
+from apps.team.models.team import Team
+from apps.base.decorators import validate_team
+from apps.invite.utils.create_invite_token import create_invite_token
+from apps.invite.serializers.invite_serializer import InviteCreateSerializer
+from typing import Any
+
+
+class InviteView(generics.GenericAPIView):
+    queryset = Invite.objects.all()
+    serializer_class = InviteCreateSerializer
+
+    @validate_team
+    def post(self, request: Request, *args: Any, team: Team, **kwargs: Any) -> Response:
+        request.data['payload'].update({'team': team.id})
+        serializer = self.get_serializer(data=request.data)
+        print(serializer)
+        if not serializer.is_valid():
+            return Response(data=serializer.errors)
+        invite = serializer.create(serializer.validated_data)
+        invite_id = {'inv_id': str(invite.id)}
+        invite_token = create_invite_token(invite_id)
+        return Response(data=invite_token, status=200)
