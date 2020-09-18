813595b8138fc5a9ebb3d022b1a38665c0b94a1e
Igor <uggoth@gmail.com>
2020-09-18 12:27:22 +0300

    fix pytest setup

--- apps/invite/__tests__/conftest.py
+++ apps/invite/__tests__/conftest.py
@@ -7,22 +7,22 @@ from apps.user.utils.create_token import create_access_and_refresh_token
 from django.core.management import call_command
 
 
-@pytest.fixture(scope="session")
-def django_db(django_db_setup: Any) -> None:
-    call_command('loaddata', './test_invite.json')
+@pytest.fixture(scope='module', autouse=True)
+def django_db(django_db_setup: Any, db_module: Any) -> None:
+    call_command('loaddata', './apps/invite/__tests__/fixture/test_invite.json')
 
-#
-# @pytest.fixture(scope="session")
-# def user() -> User:
-#     return User.objects.get(pk="cfc035db-abe8-4e87-97ed-f05dfa5a1d95")
-#
-#
-# @pytest.fixture(scope="session")
-# def team() -> Team:
-#     return Team.objects.get(pk="9509f1da-214f-4e25-b310-914c8920521a")
-#
-#
-# @pytest.fixture(scope="session")
-# def user_token(user: User) -> str:
-#     user_id = {'id': str(user.id)}
-#     return create_access_and_refresh_token(user_id)['access_token']
+
+@pytest.fixture()
+def user() -> User:
+    return User.objects.get(pk="cfc035db-abe8-4e87-97ed-f05dfa5a1d95")
+
+
+@pytest.fixture()
+def team() -> Team:
+    return Team.objects.get(pk="9509f1da-214f-4e25-b310-914c8920521a")
+
+
+@pytest.fixture()
+def user_token(user: User) -> str:
+    user_id = {'id': str(user.id)}
+    return create_access_and_refresh_token(user_id)['access_token']
--- apps/invite/__tests__/test_invite.py
+++ apps/invite/__tests__/test_invite.py
@@ -1,22 +1,13 @@
 import pytest
-from urllib.parse import urlencode, quote_plus
+from urllib.parse import urlencode
+
+from django.test import Client
 from django.urls import reverse
-from apps.base.constants import API_URL
-from ...team.models import Team
-from ...user.models import User
-from ...user.utils.create_token import create_access_and_refresh_token
 
 
 @pytest.mark.django_db
 class TestInvite:
-    def test_generate_invite(client, django_db_setup):
-        user = User.objects.get(pk="cfc035db-abe8-4e87-97ed-f05dfa5a1d95")
-        user_id = {'id': str(user.id)}
-        at_token = create_access_and_refresh_token(user_id)['access_token']
-        # at_token = user_token
-        params = {"at": at_token}
-        team = Team.objects.get(pk="9509f1da-214f-4e25-b310-914c8920521a")
-        result = urlencode(params, quote_via=quote_plus)
-        url = reverse(f'{API_URL}/teams/<uuid:team_id>/generate_invite/?{result}', kwargs={"team": team.id})
-        response = client.get(url, {"user": "11814d18-4e8d-45b6-b9c9-f08577d731eb"})
+    def test_generate_invite(self, client: Client, user, user_token, team):
+        url = f"{reverse('generate_invite', kwargs={'team_id': team.id})}?{urlencode({'at': user_token})}"
+        response = client.post(path=url, data={"payload": {"user": "11814d18-4e8d-45b6-b9c9-f08577d731eb"}})
         assert response.status_code == 200
