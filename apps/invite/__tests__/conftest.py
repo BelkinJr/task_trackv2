825a6c14191bd6f7738ac557834b1692947ac212
Vitaly <belkin.jr.nvk@gmail.com>
2020-09-18 20:23:56 +1200

    requirements updated

--- apps/invite/__tests__/conftest.py
+++ apps/invite/__tests__/conftest.py
@@ -8,21 +8,21 @@ from django.core.management import call_command
 
 
 @pytest.fixture(scope="session")
-def django_db_setup(django_db_setup: Any, db_module: Any) -> None:
+def django_db(django_db_setup: Any) -> None:
     call_command('loaddata', './test_invite.json')
 
-
-@pytest.fixture(scope="session")
-def user() -> User:
-    return User.objects.get(pk="cfc035db-abe8-4e87-97ed-f05dfa5a1d95")
-
-
-@pytest.fixture(scope="session")
-def team() -> Team:
-    return Team.objects.get(pk="9509f1da-214f-4e25-b310-914c8920521a")
-
-
-@pytest.fixture(scope="session")
-def user_token(user: User) -> str:
-    user_id = {'id': str(user.id)}
-    return create_access_and_refresh_token(user_id)['access_token']
+#
+# @pytest.fixture(scope="session")
+# def user() -> User:
+#     return User.objects.get(pk="cfc035db-abe8-4e87-97ed-f05dfa5a1d95")
+#
+#
+# @pytest.fixture(scope="session")
+# def team() -> Team:
+#     return Team.objects.get(pk="9509f1da-214f-4e25-b310-914c8920521a")
+#
+#
+# @pytest.fixture(scope="session")
+# def user_token(user: User) -> str:
+#     user_id = {'id': str(user.id)}
+#     return create_access_and_refresh_token(user_id)['access_token']
